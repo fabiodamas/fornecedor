9621d8cde606a8a94c321293e4908aedd086f47a
Fábio Damas <fabio_damas_42@hotmail.com>
2016-10-09 18:21:40 -0300

    Commit Inicial

---
+++ src/com/fiap/exemplos/ws/ProdutoWeb.java
@@ -0,0 +1,74 @@
+package com.fiap.exemplos.ws;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.xml.ws.WebServiceContext;
+
+
+@WebService
+public class ProdutoWeb {
+
+	static private List<Produto> produtos;
+	
+	@Resource
+	WebServiceContext wsctx;
+	
+	static{
+		produtos = new ArrayList<Produto>();
+		
+		Produto a = new Produto();
+		a.setCodigo(1);
+		a.setDescricao("Camisa Cavalera Amarela");
+		a.setValor(30.3);
+		produtos.add(a);
+		
+		Produto b = new Produto();
+		b.setCodigo(2);
+		b.setDescricao("Camisa Infatil Vermelha");
+		b.setValor(40.5);
+		produtos.add(b);
+		
+		Produto c = new Produto();
+		c.setCodigo(3);
+		c.setDescricao("Bermuda Tommy Hilfiger Lisa Azul P");
+		c.setValor(30.5);
+		produtos.add(c);
+		
+	}	
+	
+	
+	@WebMethod(exclude=true)
+	public boolean autenticado(String usuario, String senha){
+		if (usuario.equals("loja") && senha.equals("123")) return true;
+		else return false;
+	}
+
+	@WebMethod
+	public String novo(@WebParam(name="produto", header=false) Produto a,
+			@WebParam(name="Username", header=true) String usuario,
+			@WebParam(name="Password", header=true) String senha		
+			) {
+		if (autenticado(usuario,senha)){
+			produtos.add(a);
+			return "Produto cadastrado com sucesso";
+		}else{
+			return "Falha na autenticação";
+		}
+	}
+
+	@WebMethod
+	public List<Produto> listar(
+			@WebParam(name="Username", header=true) String usuario,
+			@WebParam(name="Password", header=true) String senha) throws Exception {
+		if (autenticado(usuario,senha)){
+			return produtos;
+		} else{
+			throw new Exception("Falha na autenticação");
+		}
+	}
+}
